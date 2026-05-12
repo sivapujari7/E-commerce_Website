dc776d6a62fc893f7e2236d839aa6bb45d78cb17
sivapujari7 <sivashiva873@gmail.com>
2026-05-12 22:25:45 +0530

    Fixed backend server

--- Back_end/server.js
+++ Back_end/server.js
@@ -1,23 +1,27 @@
 const express = require("express");
 
 const cors = require("cors");
+
 const mongoose = require("mongoose");
+
 const authRoutes = require("./routes/authRoutes");
+
 require("dotenv").config();
 
 const app = express();
 
+
 // MIDDLEWARE
 
 app.use(cors());
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/auth", authRoutes);
-
-
-app.use(cors());
 
 app.use(express.json());
+
+
+// ROUTES
+
+app.use("/api/auth", authRoutes);
+
 
 // TEST ROUTE
 
@@ -27,17 +31,7 @@ app.get("/", (req,res)=>{
 
 });
 
-// PORT
 
-const PORT = process.env.PORT || 5000;
-
-// SERVER
-
-app.listen(PORT, ()=>{
-
-  console.log(`Server Running On Port ${PORT}`);
-
-});
 // DATABASE CONNECTION
 
 mongoose.connect(process.env.MONGO_URI)
@@ -49,5 +43,19 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((error)=>{
 
   console.log(error);
+
+});
+
+
+// PORT
+
+const PORT = process.env.PORT || 5000;
+
+
+// SERVER
+
+app.listen(PORT, ()=>{
+
+  console.log(`Server Running On Port ${PORT}`);
 
 });
